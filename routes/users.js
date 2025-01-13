83a9e72fa37bfc6cd4d1d6eafb9708f38ce1e7f5
lexis-webb <lexiswebb.design@gmail.com>
2025-01-12 22:19:46 -0500

    added users

--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,32 @@ const router = express.Router();
 
 const users = [
     {
-        "firstName" : "Kim",
-        "lastname" : "Washington",
-        "age" : 17
+        firstName : "Kim",
+        lastname : "Washington",
+        age : 17,
+        occupation : "Clerk"
+    },
+    {
+        firstName : "Johnny",
+        lastname : "Fields",
+        age : 37,
+        occupation : "Baker"
     }
 ]
 
 router.get('/', (req,res) => {
-    res.send('Hello');
+    console.log(users);
+
+    res.send(users);
 });
 
+router.post('/', (req, res) => {
+  const user = req.body;
+
+    users.push(user);
+
+    res.send(`User with the name ${user.firstName} added to the Database`);
+});
 
 
 export default router;
